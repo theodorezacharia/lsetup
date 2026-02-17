9712cb957207c9417c988d5d999081b72866c489
theodore.zacharia <theodore.zacharia@ten10.com>
2026-02-17 12:25:24 +0000

    major update to windows variant

--- src/lsetup.ps1
+++ src/lsetup.ps1
@@ -2,12 +2,14 @@
 Author: Theodore Zacharia
 V0.1 - 19/07/2022 - Initial Release of Windows PowerShell version, from V0.2 Bourne shell version
 V0.2 - 16/06/2023 - Change top level structure in cfg file to an array form
+V0.5 - 17/02/2026 - Major update to use modern PS facilities and logic, but deviates a lot from the bash version
 
 Execute instructions in the json config file
 Config files are in order of preference: Machine name specific, o/s specific, default.
 
-MANDATORY: This script uses the excellent 'jq' utility,
-        which MUST be available for the script to work
+NO LONGER MANDATORY: This script used to use the excellent 'jq' utility,
+        but the recent (2026) changes now use Windows internal features
+        so no longer need js on the Windows platform
 
 WINDOWS SPECIFIC:
 You might need to do the following in an Admin Powershell script:
@@ -15,137 +17,111 @@ You might need to do the following in an Admin Powershell script:
     or to avoid prompts each time you try to run
     Set-ExecutionPolicy Bypass -Scope CurrentUser
 #>
+param (
+    [Parameter(Position = 0)]
+    [string]$Service,
+
+    [Parameter(Position = 1)]
+    [string]$Type,
+
+    # This captures EVERYTHING else after Service and Type into an array
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$ExtraArgs,    
+
+    [Alias("c")]
+    [string]$ConfigForce,
+
+    [Alias("f")]
+    [switch]$ForceIt,
+
+    [Alias("t")]
+    [switch]$Trace,
+
+    [Alias("d")]
+    [switch]$NoAutoConfig,
+
+    [Alias("h")]
+    [switch]$Help
+)
+
+if ( $TRACE ) {
+    Write-Host "DEBUG: Service is [$Service]" -ForegroundColor Magenta
+    Write-Host "DEBUG: Type is [$Type]" -ForegroundColor Magenta
+    Write-Host "DEBUG: Remaining Args: $($ExtraArgs -join ', ')" -ForegroundColor Magenta
+}
 
 <# Globals #>
 $LSETUPCFG='lsetup.cfg'    # the default lsetup config file
 $MNTFILTR=""
-$FORCEIT=0
-$TRACE=0
-$TRUNSTEPS='lsetup.tmp.tmp'
 $FINDCONFIGFILE=1
 $LSETUPCFGFORCE=""
 $POSARGSTART=0
 
-<# Mainline #>
-$TRACE=0
-$i2=0
+# --- Functions ---
+function Get-LSetupConfig {
+    param($ForcedPath, $AutoLookup)
 
-if ($TRACE -gt 0 ) { write-host "There are a total of $($args.count) arguments" }
+    # 1. If user forced a file via -c
+    if ($ForcedPath) { return $ForcedPath }
 
-for ( $i = 0; $i -lt $args.count; $i++ ) {
-    if ($TRACE -gt 0 ) { write-host "Argument $i is $($args[$i])" }
+    # 2. If Auto-lookup is disabled via -d
+    $defaultFile = "lsetup.cfg"
+    if (-not $AutoLookup) { return $defaultFile }
 
-    if ( $args[$i] -eq '-t' ) {
-        write-host "setting -t NOTE -T is same in PowerShell"
-        $TRACE=1
-        $i2=$i2+1
-    }
-    elseif ( $args[$i] -eq '-c' ) {
-        $i1=$i+1
-        write-host "setting -c with $($args[$i1])"
-        $LSETUPCFGFORCE=$($args[$i1])
-        $i=$i+1
-        $i2=$i2+1
-    }
-    elseif ( $args[$i] -eq '-f' ) {
-        $FORCEIT=1
-        $i2=$i2+1
-    }
-    elseif ( $args[$i] -eq '-d' ) {
-        $FINDCONFIGFILE=0
-        $i2=$i2+1
-    }
-    else
-    {
-        if ( $POSARGSTART -eq 0 ) { $POSARGSTART=$i2; }
+    # 3. Priority list: Machine > OS > Default
+    $targets = @(
+        "lsetup.$($env:COMPUTERNAME).cfg",
+        "lsetup.$($env:OS).cfg",
+        $defaultFile
+    )
+
+    foreach ($file in $targets) {
+        if (Test-Path $file) { return $file }
     }
 
-    # if not one of the above must be a positional one, so set here
+    return $defaultFile
 }
 
-if ($TRACE -gt 0) {
+# --- Mainline ---
+<# Mainline #>
+
+$LSETUPCFG = Get-LSetupConfig -ForcedPath $ConfigForce -AutoLookup (-not $NoAutoConfig)
+
+if ($TRACE) {
     write-host "trace is on ($trace)"
     write-host "Starting at $(get-date -Format F)"
+    write-host "Using config LSETUPCFG=$LSETUPCFG" -ForegroundColor Cyan
 }
 
+# Load data once
+$Data = Get-Content -Raw $LSETUPCFG | ConvertFrom-Json
+$MYARG1=$Service
+$MYARG2=$Type
 
+if ( $TRACE ) { write-host "MYARG1=${MYARG1} , MYARG2=${MYARG2}" }
 
-if ( $args.count -eq 0)
-{
-    write-host "see option -h for help"
-    exit 1
+if ([string]::IsNullOrWhiteSpace($MYARG2)) {
+    # Only match if the JSON type is ALSO null or empty
+    $TRUN = $Data | Where-Object { $_.service -eq $MYARG1 -and [string]::IsNullOrEmpty($_.type) }
+} else {
+    $TRUN = $Data | Where-Object { $_.service -eq $MYARG1 -and $_.type -eq $MYARG2 }
 }
 
+if ( $TRACE ) { $TRUN.steps | Out-String }
 
-# find which config file to use
-if ( $FINDCONFIGFILE -eq 1 )
-{
-    if ( "$LSETUPCFGFORCE" -ne "" )
-    {
-        $LSETUPCFG=$LSETUPCFGFORCE
-    }
-    else
-    {
-        # Look for Linux/Windows/Mac etc
-        $CMPOS="$env:os"
-        $CHKLSETUP="lsetup.${CMPOS}.cfg"
-        if ( Test-Path "$CHKLSETUP" )
-        {
-            $LSETUPCFG=$CHKLSETUP
-        }
-
-        # Look for a machine name specific file
-        $CMPNM="$env:computername"
-        write-host "on $CMPNM"
-        $CHKLSETUP="lsetup.${CMPNM}.cfg"
-        if ( Test-Path "$CHKLSETUP" )
-        {
-            $LSETUPCFG=$CHKLSETUP
+if ($null -ne $TRUN) {
+    if ($TRACE) { Write-Host "Executing steps for $($TRUN.service)..." -ForegroundColor Cyan }
+    
+    foreach ($step in $TRUN.steps) {
+        # Skip comments or empty lines
+        if ($step.StartsWith("#") -or [string]::IsNullOrWhiteSpace($step)) { continue }
+        if ( $TRACE ) { Write-Host "Running: $step" -ForegroundColor Gray }
+        try {
+            # Invoke-Expression runs the string as a PowerShell command
+            Invoke-Expression $step
+        } catch {
+            Write-Error "Failed to execute step: $step. Error: $_"
         }
     }
 }
 
-if ( $TRACE -gt 0 ) { write-host "using LSETUPCFG=$LSETUPCFG" }
-
-# not sure how to get/shift arguments in the official way, so implemented equiv of OPTIND with shift
-
-$MYARG1=$args[$POSARGSTART]
-$MYARG2=$args[$POSARGSTART+1]
-
-if ( $TRACE -gt 0 ) { write-host "MYARG1=${MYARG1} , MYARG2=${MYARG2}" }
-
-
-# handle 1 parameter, 2 linked (requires type in the cfg) or 2 where 2nd is just data (type is empty in this case in the cfg)
-if ( "$MYARG2" -ne "" )
-{
-    # if TWO parameters provided, need to handle ones where 2nd param may be something to pass into script
-    # this is the case if the type in config is EMPTY
-    jq --arg myjarg1 "$MYARG1" --arg myjarg2 "$MYARG2" ' .[] | select ( .service == $myjarg1 ) | select ( .type == $myjarg2 ) ' $LSETUPCFG | Tee-Object -Variable TRUN > Out-Null
-    if ( $TRACE -gt 0 ) { write-host "Steps: $TRUN" }
-
-    if ( "$TRUN" -eq "" )
-    {
-        jq --arg myjarg1 "$MYARG1" ' .[] | select ( .service == $myjarg1 ) | select ( .type == \"\" ) ' $LSETUPCFG | Tee-Object -Variable TRUN > Out-Null
-    }
-}
-else
-{
-    # if ONE parameter passed, need to only select ones with no type
-    jq --arg myjarg1 "$MYARG1" ' .[] | select ( .service == $myjarg1 )  | select ( .type == \"\" ) ' $LSETUPCFG | Tee-Object -Variable TRUN > Out-Null
-}
-
-if ( $TRACE -gt 1 ) { write-host "Steps: $TRUN" }
-echo "$TRUN" | jq -r ' .steps[] ' > $TRUNSTEPS
-
-if ( $TRACE -gt 1 ) { cat $TRUNSTEPS }
-
-
-ForEach ($ASTEP in Get-Content $TRUNSTEPS)
-{
-    if ( $TRACE -gt 0 ) { write-host "running: $ASTEP" }
-    # force to get input from terminal
-    Invoke-Expression $ASTEP
-}
-
-rm $TRUNSTEPS
-rm Out-Null
